f384019c19ce1065542a99acf311521ed9104053
athellen <haakinyi@gmail.com>
2019-12-23 14:42:10 +0300

    add changes to res

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,10 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.static("dist"));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../dist", "index.html"));
+});
 app.use("/api", (req, res) => {
   res.send("working!");
 });
